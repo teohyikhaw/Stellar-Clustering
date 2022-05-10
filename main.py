db22919a3925c495a2d75e3ad784bf7599b65916
Yik Haw Teoh <teohyikhaw@gmail.com>
2022-05-09 23:02:10 -0700

    Implemented PCA
    Note: standardization has not been done on x

--- main.py
+++ main.py
@@ -2,5 +2,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import scipy
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv('star_classification.csv')
+features = ['obj_ID','alpha','delta','u','g','r','i','z','run_ID','rerun_ID','cam_col','field_ID','spec_obj_ID','redshift','plate','MJD','fiber_ID']
+
+x = np.array([df[name] for name in features])
+#normalize x here
+#x = StandardScaler().fit_transform(x)
+
+xbar = [np.mean(temp) for temp in x]
+
+for i in range(17):
+    x[i]=x[i]-xbar[i]
+
+U, S, V = np.linalg.svd(x, full_matrices=True)
+Y = U*S
+print(U)
